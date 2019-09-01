fcf7d13872078b6e2e77d91bb8f248f2b3c4cb63
Rong <deamerrong123@gmail.com>
2019-08-31 22:19:35 -0400

    update

--- drop_action.js
+++ drop_action.js
@@ -1,8 +1,22 @@
-const Tm = 05; // the time for executing
-const Ts = 30;
+const Tm = 41; // the time for executing
+const Ts = 05;
 var dT; // Time before execution
+var logInField = document.querySelector("input[id = 'loginsubmitbutton']");
+var TomorrowButton = document.querySelector("input[value = 'TOMORROW']");
+var TechRoom = document.querySelector('a[onclick]'); // this is not working
 
 var time = new Date();
+
+
+let Time = {
+   getDate : function(){
+    t = time.toString().split(' ');
+    Str = t[1] + ' '+t[2] + ','+t[3];
+    return Str;
+   }
+   
+
+}
 
 function triggerEvent(el, type){
     if ('createEvent' in document) {
@@ -18,13 +32,24 @@ function triggerEvent(el, type){
 }
 }
 
-function myfunction(){
-    elem = document.querySelector('a[href="33386988.html"]');
+function ClkTomor(){
+    // elem = document.querySelector('a[href="33386988.html"]');
+    elem = TomorrowButton;
     triggerEvent(elem,'click'); 
 }
 
-function act(t){
-    setTimeout(myfunction,t);
+function ClkThchRoom(TimeStr,TimeEnd){
+
+    // elem = TechRoom;
+    // triggerEvent(elem,'click');
+    dayviewer('1567310400','1567396799','13','');
+}
+
+function act(b,e,t){
+    // setTimeout(ClkTomor,t);
+    begin = b;
+    end = e;
+    setTimeout(ClkThchRoom(begin,end),t);
 }
 
 dT = ((Tm-time.getMinutes())*60 + (Ts - time.getSeconds()))*1000;
