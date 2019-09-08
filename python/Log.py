6127b5ed52868417162952f78f13970ff4c16b87
Rong <deamerrong123@gmail.com>
2019-09-08 10:43:37 -0400

    updated

--- python/Log.py
+++ python/Log.py
@@ -1,17 +1,39 @@
 #https://www.baruch.cuny.edu/library/reservaroom/
-import requests
-
+from urllib import request,parse
+from http.cookiejar import CookieJar
 
 class Loger:
-    header = {
+    headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-
     }
+    cookiejar = CookieJar()
+    handler =  request.HTTPCookieProcessor(cookiejar)
+    opener = request.build_opener(handler)
     username_dic = {
         'q.rong','y.bai1'
     }
-    def __int__(self,username,password):
-        pass
+
+    def __int__(self,URL,username,password):
+        self.URL = URL
+        self._username = username
+        self._password = password
+        self._data = {
+            'Username':username,
+            'Password':password
+        }
+
+
+    def log(self):
+        if self._username not in Loger.username_dic:
+            raise ValueError
+
+        if self._username in Loger.username_dic:
+            req = request.Request(self.URL,data = parse.urlencode(
+                self.data).endcode('utf-8'),headers=Loger.headers)
+            self.opener.open(req)
+
+
+
 
 
 if __name__ == '__main__':
--- python/log_GUI.py
+++ python/log_GUI.py
@@ -2,28 +2,33 @@ from tkinter import *
 from tkinter import ttk, filedialog, messagebox
 
 class logPage:
+    _username = StringVar()
+    _password = StringVar()
 
     def __init__(self,parent):
         self._parent = parent
         self._mainframe = ttk.Frame(self._parent,padding = '5 5 5 5')
+        ## mainframe
         self._mainframe.grid(row = 0 , column = 0 , sticky = (E,W,N,S))
 
+        ## userfield
         self._username_field = ttk.LabelFrame(self._mainframe,
                                           text = 'Username', padding = '5 5 5 5')
         self._username_field.grid(row = 0 , column = 0,
                                   sticky = (E,W,N,S))
-        self._username = StringVar()
-        self._password = StringVar()
+
         self._user_Entry = ttk.Entry(self._username_field,width = 20 ,
-                                     textvariable = self._username)
+                                     textvariable = logPage._username)
         self._user_Entry.grid(row = 0 , column = 0, sticky = (W,E))
 
         self._password_field = ttk.LabelFrame(self._mainframe,text = 'Password'
                                           ,padding = '5 5 5 5')
         self._password_field.grid(row = 1, column = 0,sticky = (W,E))
         self._password_Entry = ttk.Entry(self._password_field,width = 20,
-                                         textvariable = self._password)
+                                         textvariable = logPage._password)
         self._password_Entry.grid(row = 0 , column = 0 , sticky = (W,E))
+
+
 
 
 
